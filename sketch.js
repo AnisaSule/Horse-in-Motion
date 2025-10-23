2cb77507f0c2514031c000cfd96a803d9ae89542
AnisaSule <sulea@ocadu.ca>
2025-10-22 23:34:21 -0400

    new if statement

--- sketch.js
+++ sketch.js
@@ -14,13 +14,18 @@ function setup() {
 function draw() {
   background(0);
   textSize(28); textAlign(CENTER); fill(255);
-  text("Horse In Motion", width/2, 50);
+  text("Horse In Motion", width / 2, 50);
 
-    if (rotationY < 0) scale(-1, 1);
-    image(horse, 0, 0); pop();
+  if (gyroIsEnabled) {
+    push();
+    translate(width / 2, height / 2); // Center the image
+    if (rotationY < 0) scale(-1, 1);  // Flip if tilting left
+    image(horse, 0, 0);               // Draw at center
+    pop();
   } else {
-    text("Touch screen to start", width/2, height/2);
+    text("Touch screen to start", width / 2, height / 2);
   }
+}
 
 
 function onGyroEnabled() {
