0529c485437f7955d3689bbe77ebbc2d29dd3c23
AnisaSule <sulea@ocadu.ca>
2025-10-22 22:06:34 -0400

    still not working

--- sketch.js
+++ sketch.js
@@ -1,47 +1,33 @@
+
+
 let horse;
-let devicerotationY = 0;
+
+function preload() {
+    horse = loadImage("Assets/HorseInMotion.gif"); 
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    lockGestures();
-    enableGyroTap("Touch your device to see the horse in motion");
     imageMode(CENTER);
-}
-
-function preload() {
-    horse = loadImage("Assets/HorseInMotion.gif");
 }
 
 function draw() {
     background(0);
 
-    // Only draw if gyro is enabled
-    if (gyroIsEnabled) {
-        // Update devicerotationY from rotation event
-        devicerotationY = rotationY;
-        
-        // Draw title text
+    if (rotationY !== undefined) {
         textSize(28);
         textAlign(CENTER);
-        text("Horse in Motion", width/2, height/2 - 100);
+        fill(255);
+        text("Horse in Motion", width / 2, height / 2 - 100);
 
         push();
-        translate(width/2, height/2);
-        
-        // Mirror horse based on device rotation
-        if (devicerotationY < 0) {
+        translate(width / 2, height / 2);
+
+        if (rotationY < 0) {
             scale(-1, 1);
         }
-        
-        // Draw the horse image
+
         image(horse, 0, 0);
         pop();
     }
 }
-
-// Add event handler for device rotation
-window.addEventListener('deviceorientation', function(event) {
-    devicerotationY = event.beta;
-});
-
-
