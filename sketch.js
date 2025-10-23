60c4c3a7ac665065a396ddc29a38fab6e3182af3
AnisaSule <sulea@ocadu.ca>
2025-10-22 23:16:31 -0400

    code revanp

--- sketch.js
+++ sketch.js
@@ -1,45 +1,29 @@
-let horse;
-let gyroIsEnabled = false;
+let horse, gyroIsEnabled = false;
 
 function preload() {
-    horse = loadImage("./Assets/HorseInMotion.gif"); 
+  horse = loadImage("./Assets/HorseInMotion.gif");
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    imageMode(CENTER);
-    lockGestures();
-    // Changed from enableGyroMode to enableGyroTap for better user experience
-    enableGyroTap("Touch screen to enable gyroscope");
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+  enableGyroTap("Touch screen to enable gyroscope");
 }
 
 function draw() {
-    background(0);
+  background(0);
+  textSize(28); textAlign(CENTER); fill(255);
 
-    if (gyroIsEnabled) {
-        textSize(28);
-        textAlign(CENTER);
-        fill(255);
-        text("Horse in Motion", width / 2, height / 2 - 300);
-
-        push();
-        translate(width / 2, height / 2);
-
-        if (rotationY < 0) {
-            scale(-1, 1);
-        }
-
-        image(horse, 0, 0);
-        pop();
-    } else {
-        // Show instructions when gyro is not enabled
-        textSize(28);
-        textAlign(CENTER);
-        fill(255);
-        text("Touch screen to start", width / 2, height / 2);
-    }
+  if (gyroIsEnabled) {
+    text("Horse in Motion", width/2, height/2 - 300);
+    push(); translate(width/2, height/2);
+    if (rotationY < 0) scale(-1, 1);
+    image(horse, 0, 0); pop();
+  } else {
+    text("Touch screen to start", width/2, height/2);
+  }
 }
 
 function onGyroEnabled() {
-    gyroIsEnabled = true;
+  gyroIsEnabled = true;
 }
