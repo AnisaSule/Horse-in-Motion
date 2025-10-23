21be2c3289f9c437deced47a91846fabb69b5ad8
AnisaSule <sulea@ocadu.ca>
2025-10-22 23:40:58 -0400

    tru

--- sketch.js
+++ sketch.js
@@ -1,33 +1,24 @@
-let horse, gyroIsEnabled = false;
+let horse;
 
 function preload() {
-  horse = loadImage("./Assets/HorseInMotion.gif");
+    horse = loadImage("Assets/HorseInMotion.gif");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER);
-  enableGyroTap("Touch screen to enable gyroscope");
-  lockGestures();
+    createCanvas(windowWidth, windowHeight);
+    imageMode(CENTER);
+    lockGestures();
+    enableGyroTap("Touch to start");
 }
 
 function draw() {
-  background(0);
-  textSize(28); textAlign(CENTER); fill(255);
-  text("Horse In Motion", width / 2, 50);
-
-  if (gyroIsEnabled) {
-    push();
-    translate(width / 2, height / 2); // Center the image
-    if (rotationY < 0) scale(-1, 1);  // Flip if tilting left
-    image(horse, 0, 0);               // Draw at center
-    pop();
-  } else {
-    text("Touch screen to start", width / 2, height / 2);
-  }
-}
-
-
-function onGyroEnabled() {
-  gyroIsEnabled = true;
+    background(0);
+    
+    if (gyroIsEnabled) {
+        push();
+        translate(width/2, height/2);
+        if (rotationY < 0) scale(-1, 1);
+        image(horse, 0, 0);
+        pop();
+    }
 }
