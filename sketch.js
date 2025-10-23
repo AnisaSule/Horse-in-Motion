1b26cc8de88a27de498ed273480383bca83acb40
AnisaSule <sulea@ocadu.ca>
2025-10-22 22:02:04 -0400

    AI HELP UPDATE

--- sketch.js
+++ sketch.js
@@ -1,37 +1,47 @@
 let horse;
 let devicerotationY = 0;
 
-function setup() 
-{
-	createCanvas(windowWidth,windowHeight);
-	lockGestures();
-	enableGyroTap("Touch your device to see the horse in motion")
-	imageMode(CENTER);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    lockGestures();
+    enableGyroTap("Touch your device to see the horse in motion");
+    imageMode(CENTER);
 }
 
-function preload(){
- horse = loadImage ("Assets/HorseInMotion.gif")
+function preload() {
+    horse = loadImage("Assets/HorseInMotion.gif");
 }
 
-function draw()
-{
-background (0);
+function draw() {
+    background(0);
 
-if (gyroIsEnabled) {
-textSize (28);
-text ("horse in Motion", width/2, height/2 - 100);
-}
-
-push ();
-translate (width/2, height/2);
-
-if (devicerotationY < 0) {
- scale(-1, 1); 
- }
+    // Only draw if gyro is enabled
+    if (gyroIsEnabled) {
+        // Update devicerotationY from rotation event
+        devicerotationY = rotationY;
         
-// Draw the horse image
-image(horse, 0, 0);
-pop(); 
+        // Draw title text
+        textSize(28);
+        textAlign(CENTER);
+        text("Horse in Motion", width/2, height/2 - 100);
+
+        push();
+        translate(width/2, height/2);
+        
+        // Mirror horse based on device rotation
+        if (devicerotationY < 0) {
+            scale(-1, 1);
+        }
+        
+        // Draw the horse image
+        image(horse, 0, 0);
+        pop();
     }
+}
+
+// Add event handler for device rotation
+window.addEventListener('deviceorientation', function(event) {
+    devicerotationY = event.beta;
+});
 
 
